ec55450133010b2e374f6ec5e2181686301a7537
Juan Ignacio Londra <londrajuan@gmail.com>
2024-06-18 05:32:39 -0300

    add ngx tippy option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,8 @@ import { InputNumberModule } from 'primeng/inputnumber';
 import { FormsModule } from '@angular/forms';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { CheckboxModule } from 'primeng/checkbox';
+import { NgxTippyModule } from 'ngx-tippy-wrapper';
+import { SelectButtonModule } from 'primeng/selectbutton';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -23,6 +25,8 @@ import { CheckboxModule } from 'primeng/checkbox';
     TooltipModule,
     InputNumberModule,
     CheckboxModule ,
+    NgxTippyModule,
+    SelectButtonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -36,6 +40,12 @@ export class AppComponent implements AfterViewChecked {
   previousChangeDetectionCount = 0;
   cyclesAfterKeyPress = 0;
   checked:boolean = true;
+  selectedState  = 0;
+  stateOptions: any[] = [
+    { label: 'no tooltip', value: 0 },
+    { label: 'use primeng tooltip', value: 1 },
+    { label: 'use ngxTippy tooltip', value: 2 }
+];
 
   constructor(private cdRef: ChangeDetectorRef) {}
 
